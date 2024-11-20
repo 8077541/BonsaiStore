3aea67c949490e6fb709dc929254f4f3a5cea7c2
8077541 <dominikjaniakxyz@gmail.com>
2024-11-20 23:23:57 +0100

    Dto Progress

--- BonsaiApi/DTOs/ModelDto/CreateOrderDto.cs
+++ BonsaiApi/DTOs/ModelDto/CreateOrderDto.cs
@@ -2,11 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BonsaiStore.Models;
 
 namespace BonsaiApi.DTOs.ModelDto
 {
     public class CreateOrderDto
     {
-        
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string Zip { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+        public string PaymentMethod { get; set; } = "Cash";
+        public string ShippingMethod { get; set; } = "Standard";
+
+        public List<ItemModel> Items { get; set; } = new List<ItemModel>();
     }
 }
